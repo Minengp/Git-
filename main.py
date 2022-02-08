6a4335a824391006d84d619c7619bb9db1e2ca9c
Mineng <mineng.top5@yandex.ru>
2022-02-08 18:00:27 +0500

    Git и случайные окружности

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from PyQt5.QtCore import QRect
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5 import uic
-from random import randrange
+from random import randrange, randint
+from UI import Ui_MainWindow
 
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
-class Main(QMainWindow):
+class Main(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.btn.clicked.connect(self.draw)
         self.flag = False
 
@@ -21,7 +22,7 @@ class Main(QMainWindow):
         if self.flag:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(randint(0, 256), randint(0, 256), randint(0, 256)))
             x = randrange(1, 300)
             y = randrange(1, 300)
             r = randrange(1, 300)
